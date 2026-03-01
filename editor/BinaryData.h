db925ba94d392077f26a0d4acd7a55c2fa4501ad
Moebytes <pie4chan@gmail.com>
2026-02-28 23:05:22 -0500

    fix?

--- editor/BinaryData.h
+++ editor/BinaryData.h
@@ -7,4 +7,7 @@ namespace BinaryData
     extern const char*  chopper_py;
     const int           chopper_pySize = 3256;
 
+    extern const char*  webview_files_zip;
+    const int           webview_files_zipSize = 143238;
+
 }
